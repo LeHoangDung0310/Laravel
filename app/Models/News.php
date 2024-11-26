660973f99b5299ace0469c4a188c6fef04cd70ab
haraherri <giahuy280703@gmail.com>
2024-11-26 12:41:49 +0700

    feat: News Post - Handling Tags and Show Data Index

--- app/Models/News.php
+++ app/Models/News.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class News extends Model
 {
     use HasFactory;
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class, 'news_tags');
+    }
 }
