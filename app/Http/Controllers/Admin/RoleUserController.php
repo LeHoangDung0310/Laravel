eaed5efdcc61bcd86a8d266921079ec4d6477f2e
Dương Văn Bảo <tidumong789@gmail.com>
2024-12-01 21:32:43 +0700

    Role User System- Working with Create Feature

--- app/Http/Controllers/Admin/RoleUserController.php
+++ app/Http/Controllers/Admin/RoleUserController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\AdminRoleUserStoreRequest;
+use App\Models\Admin;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
 
 class RoleUserController extends Controller
 {
@@ -19,17 +23,32 @@ class RoleUserController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    public function create() : View
     {
-        //
+        $roles = Role::all();
+        return view('admin.role-user.create', compact('roles'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(AdminRoleUserStoreRequest $request) : RedirectResponse
     {
-        //
+        $user = new Admin();
+        $user->image = '';
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = bcrypt($request->password);
+        $user->status = 1;
+        $user->save();
+
+        /** assign the role to user */
+        $user->assignRole($request->role);
+
+        toast(__('Created Successfully!'), 'success');
+
+        return redirect()->route('admin.role-users.index');
+
     }
 
     /**
