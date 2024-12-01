5f0ad373fce218dc336bb5b5f25cc0ba9dcff641
Dương Văn Bảo <tidumong789@gmail.com>
2024-12-01 23:34:31 +0700

    News Section - Show News Depending on Admin id and permissions

--- resources/views/admin/pending-news/index.blade.php
+++ resources/views/admin/pending-news/index.blade.php
@@ -3,13 +3,17 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <h1>{{ __('Pending News') }}</h1>
+            <h1>{{ __('News') }}</h1>
         </div>
 
         <div class="card card-primary">
             <div class="card-header">
-                <h4>{{ __('All Pending') }}</h4>
-
+                <h4>{{ __('All News') }}</h4>
+                <div class="card-header-action">
+                    <a href="{{ route('admin.news.create') }}" class="btn btn-primary">
+                        <i class="fas fa-plus"></i> {{ __('Create new') }}
+                    </a>
+                </div>
             </div>
 
             <div class="card-body">
@@ -26,11 +30,20 @@
                 <div class="tab-content tab-bordered" id="myTab3Content">
                     @foreach ($languages as $language)
                         @php
-                            $news = \App\Models\News::with('category')
+                            if(canAccess(['news all-access'])){
+                                $news = \App\Models\News::with('category')
                                 ->where('language', $language->lang)
-                                ->where('is_approved', 0)
+                                ->where('is_approved', 1)
                                 ->orderBy('id', 'DESC')
                                 ->get();
+                            }else {
+                                $news = \App\Models\News::with('category')
+                                ->where('language', $language->lang)
+                                ->where('is_approved', 1)
+                                ->where('auther_id', auth()->guard('admin')->user()->id)
+                                ->orderBy('id', 'DESC')
+                                ->get();
+                            }
                         @endphp
                         <div class="tab-pane fade show {{ $loop->index === 0 ? 'active' : '' }}"
                             id="home-{{ $language->lang }}" role="tabpanel" aria-labelledby="home-tab2">
@@ -45,8 +58,10 @@
                                                 <th>{{ __('Image') }}</th>
                                                 <th>{{ __('Title') }}</th>
                                                 <th>{{ __('Category') }}</th>
-                                                <th>{{ __('Approve') }}</th>
-
+                                                <th>{{ __('In Breaking') }}</th>
+                                                <th>{{ __('In Slider') }}</th>
+                                                <th>{{ __('In Popular') }}</th>
+                                                <th>{{ __('Status') }}</th>
                                                 <th>{{ __('Action') }}</th>
                                             </tr>
                                         </thead>
@@ -60,17 +75,43 @@
 
                                                     <td>{{ $item->title }}</td>
                                                     <td>{{ $item->category->name }}</td>
+
                                                     <td>
-                                                        <form action="" id="approve_form">
-                                                            <input type="hidden" name="id" value="{{ $item->id }}">
-                                                            <div class="form-group">
-                                                                <select name="is_approve" class="form-control" id="approve-input">
-                                                                    <option value="0">{{ __('Pending') }}</option>
-                                                                    <option value="1">{{ __('Approved') }}</option>
-                                                                </select>
-                                                            </div>
-                                                        </form>
+                                                        <label class="custom-switch mt-2">
+                                                            <input {{ $item->is_breaking_news === 1 ? 'checked' : '' }}
+                                                                data-id="{{ $item->id }}" data-name="is_breaking_news"
+                                                                value="1" type="checkbox" class="custom-switch-input toggle-status">
+                                                            <span class="custom-switch-indicator"></span>
+                                                        </label>
                                                     </td>
+
+                                                    <td>
+                                                        <label class="custom-switch mt-2">
+                                                            <input {{ $item->show_at_slider === 1 ? 'checked' : '' }}
+                                                                data-id="{{ $item->id }}" data-name="show_at_slider"
+                                                                value="1" type="checkbox" class="custom-switch-input toggle-status">
+                                                            <span class="custom-switch-indicator"></span>
+                                                        </label>
+                                                    </td>
+
+                                                    <td>
+                                                        <label class="custom-switch mt-2">
+                                                            <input {{ $item->show_at_popular === 1 ? 'checked' : '' }}
+                                                                data-id="{{ $item->id }}" data-name="show_at_popular"
+                                                                value="1" type="checkbox" class="custom-switch-input toggle-status">
+                                                            <span class="custom-switch-indicator"></span>
+                                                        </label>
+                                                    </td>
+
+                                                    <td>
+                                                        <label class="custom-switch mt-2">
+                                                            <input {{ $item->status === 1 ? 'checked' : '' }}
+                                                                data-id="{{ $item->id }}" data-name="status"
+                                                                value="1" type="checkbox" class="custom-switch-input toggle-status">
+                                                            <span class="custom-switch-indicator"></span>
+                                                        </label>
+                                                    </td>
+
 
                                                     <td>
                                                         <a href="{{ route('admin.news.edit', $item->id) }}"
@@ -117,27 +158,25 @@
         @endforeach
 
         $(document).ready(function(){
+            $('.toggle-status').on('click', function(){
+                let id = $(this).data('id');
+                let name = $(this).data('name');
+                let status = $(this).prop('checked') ? 1 : 0;
 
-            $('#approve-input').on('change', function(){
-                $('#approve_form').submit();
-            });
-
-            $('#approve_form').on('submit', function(e){
-                e.preventDefault();
-
-                let data = $(this).serialize();
                 $.ajax({
-                    method: 'PUT',
-                    url: "{{ route('admin.approve.news') }}",
-                    data: data,
+                    method: 'GET',
+                    url: "{{ route('admin.toggle-news-status') }}",
+                    data: {
+                        id:id,
+                        name:name,
+                        status:status
+                    },
                     success: function(data){
                         if(data.status === 'success'){
                             Toast.fire({
                                 icon: 'success',
                                 title: data.message
                             })
-
-                            window.location.reload();
                         }
                     },
                     error: function(error){
