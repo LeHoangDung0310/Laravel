b34284c4a960716aa5e0270b3186990784858fce
Dương Văn Bảo <tidumong789@gmail.com>
2024-12-01 23:36:54 +0700

    News Section - Fix Error Show News Depending on Admin id and permissions

--- resources/views/admin/pending-news/index.blade.php
+++ resources/views/admin/pending-news/index.blade.php
@@ -3,136 +3,82 @@
 @section('content')
     <section class="section">
         <div class="section-header">
-            <h1>{{ __('News') }}</h1>
+            <h1>{{ __('Pending News') }}</h1>
         </div>
 
         <div class="card card-primary">
             <div class="card-header">
-                <h4>{{ __('All News') }}</h4>
-                <div class="card-header-action">
-                    <a href="{{ route('admin.news.create') }}" class="btn btn-primary">
-                        <i class="fas fa-plus"></i> {{ __('Create new') }}
-                    </a>
-                </div>
+                <h4>{{ __('All Pending') }}</h4>
+
             </div>
 
+            @php
+                if(canAccess(['news all-access'])){
+                    $news = \App\Models\News::with('category')
+                    ->where('is_approved', 0)
+                    ->orderBy('id', 'DESC')
+                    ->get();
+                }else {
+                    $news = \App\Models\News::with('category')
+                    ->where('is_approved', 0)
+                    ->where('auther_id', auth()->guard('admin')->user()->id)
+                    ->orderBy('id', 'DESC')
+                    ->get();
+                }
+            @endphp
             <div class="card-body">
-                <ul class="nav nav-tabs" id="myTab2" role="tablist">
-                    @foreach ($languages as $language)
-                        <li class="nav-item">
-                            <a class="nav-link {{ $loop->index === 0 ? 'active' : '' }}" id="home-tab2" data-toggle="tab"
-                                href="#home-{{ $language->lang }}" role="tab" aria-controls="home"
-                                aria-selected="true">{{ $language->name }}</a>
-                        </li>
-                    @endforeach
+                <div class="table-responsive">
+                    <table class="table table-striped" id="table">
+                        <thead>
+                            <tr>
+                                <th class="text-center">
+                                    #
+                                </th>
+                                <th>{{ __('Image') }}</th>
+                                <th>{{ __('Title') }}</th>
+                                <th>{{ __('Category') }}</th>
+                                <th>{{ __('Approve') }}</th>
 
-                </ul>
-                <div class="tab-content tab-bordered" id="myTab3Content">
-                    @foreach ($languages as $language)
-                        @php
-                            if(canAccess(['news all-access'])){
-                                $news = \App\Models\News::with('category')
-                                ->where('language', $language->lang)
-                                ->where('is_approved', 1)
-                                ->orderBy('id', 'DESC')
-                                ->get();
-                            }else {
-                                $news = \App\Models\News::with('category')
-                                ->where('language', $language->lang)
-                                ->where('is_approved', 1)
-                                ->where('auther_id', auth()->guard('admin')->user()->id)
-                                ->orderBy('id', 'DESC')
-                                ->get();
-                            }
-                        @endphp
-                        <div class="tab-pane fade show {{ $loop->index === 0 ? 'active' : '' }}"
-                            id="home-{{ $language->lang }}" role="tabpanel" aria-labelledby="home-tab2">
-                            <div class="card-body">
-                                <div class="table-responsive">
-                                    <table class="table table-striped" id="table-{{ $language->lang }}">
-                                        <thead>
-                                            <tr>
-                                                <th class="text-center">
-                                                    #
-                                                </th>
-                                                <th>{{ __('Image') }}</th>
-                                                <th>{{ __('Title') }}</th>
-                                                <th>{{ __('Category') }}</th>
-                                                <th>{{ __('In Breaking') }}</th>
-                                                <th>{{ __('In Slider') }}</th>
-                                                <th>{{ __('In Popular') }}</th>
-                                                <th>{{ __('Status') }}</th>
-                                                <th>{{ __('Action') }}</th>
-                                            </tr>
-                                        </thead>
-                                        <tbody>
-                                            @foreach ($news as $item)
-                                                <tr>
-                                                    <td>{{ $item->id }}</td>
-                                                    <td >
-                                                        <img src="{{ asset($item->image) }}" width="100" alt="">
-                                                    </td>
+                                <th>{{ __('Action') }}</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($news as $item)
+                                <tr>
+                                    <td>{{ $item->id }}</td>
+                                    <td >
+                                        <img src="{{ asset($item->image) }}" width="100" alt="">
+                                    </td>
 
-                                                    <td>{{ $item->title }}</td>
-                                                    <td>{{ $item->category->name }}</td>
+                                    <td>{{ $item->title }}</td>
+                                    <td>{{ $item->category->name }}</td>
+                                    <td>
+                                        <form action="" id="approve_form">
+                                            <input type="hidden" name="id" value="{{ $item->id }}">
+                                            <div class="form-group">
+                                                <select name="is_approve" class="form-control" id="approve-input">
+                                                    <option value="0">{{ __('Pending') }}</option>
+                                                    <option value="1">{{ __('Approved') }}</option>
+                                                </select>
+                                            </div>
+                                        </form>
+                                    </td>
 
-                                                    <td>
-                                                        <label class="custom-switch mt-2">
-                                                            <input {{ $item->is_breaking_news === 1 ? 'checked' : '' }}
-                                                                data-id="{{ $item->id }}" data-name="is_breaking_news"
-                                                                value="1" type="checkbox" class="custom-switch-input toggle-status">
-                                                            <span class="custom-switch-indicator"></span>
-                                                        </label>
-                                                    </td>
-
-                                                    <td>
-                                                        <label class="custom-switch mt-2">
-                                                            <input {{ $item->show_at_slider === 1 ? 'checked' : '' }}
-                                                                data-id="{{ $item->id }}" data-name="show_at_slider"
-                                                                value="1" type="checkbox" class="custom-switch-input toggle-status">
-                                                            <span class="custom-switch-indicator"></span>
-                                                        </label>
-                                                    </td>
-
-                                                    <td>
-                                                        <label class="custom-switch mt-2">
-                                                            <input {{ $item->show_at_popular === 1 ? 'checked' : '' }}
-                                                                data-id="{{ $item->id }}" data-name="show_at_popular"
-                                                                value="1" type="checkbox" class="custom-switch-input toggle-status">
-                                                            <span class="custom-switch-indicator"></span>
-                                                        </label>
-                                                    </td>
-
-                                                    <td>
-                                                        <label class="custom-switch mt-2">
-                                                            <input {{ $item->status === 1 ? 'checked' : '' }}
-                                                                data-id="{{ $item->id }}" data-name="status"
-                                                                value="1" type="checkbox" class="custom-switch-input toggle-status">
-                                                            <span class="custom-switch-indicator"></span>
-                                                        </label>
-                                                    </td>
+                                    <td>
+                                        <a href="{{ route('admin.news.edit', $item->id) }}"
+                                            class="btn btn-primary"><i class="fas fa-edit"></i></a>
+                                        <a href="{{ route('admin.news.destroy', $item->id) }}"
+                                            class="btn btn-danger delete-item"><i
+                                                class="fas fa-trash-alt"></i></a>
+                                        <a href="{{ route('admin.news-copy', $item->id) }}"
+                                            class="btn btn-primary"><i class="fas fa-copy"></i></i></a>
+                                    </td>
+                                </tr>
+                            @endforeach
 
 
-                                                    <td>
-                                                        <a href="{{ route('admin.news.edit', $item->id) }}"
-                                                            class="btn btn-primary"><i class="fas fa-edit"></i></a>
-                                                        <a href="{{ route('admin.news.destroy', $item->id) }}"
-                                                            class="btn btn-danger delete-item"><i
-                                                                class="fas fa-trash-alt"></i></a>
-                                                        <a href="{{ route('admin.news-copy', $item->id) }}"
-                                                            class="btn btn-primary"><i class="fas fa-copy"></i></i></a>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
-
-
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div>
-                    @endforeach
-
+                        </tbody>
+                    </table>
                 </div>
             </div>
 
@@ -143,40 +89,42 @@
 
 @push('scripts')
     <script>
-        @foreach ($languages as $language)
-            $("#table-{{ $language->lang }}").dataTable({
-                "columnDefs": [
-                    {
-                        "sortable": false,
-                        "targets": [2, 3]
-                    }
-                ],
-                "order": [
-                    [0, 'desc']
-                ]
-            });
-        @endforeach
+
+        $("#table").dataTable({
+            "columnDefs": [
+                {
+                    "sortable": false,
+                    "targets": [2, 3]
+                }
+            ],
+            "order": [
+                [0, 'desc']
+            ]
+        });
+
 
         $(document).ready(function(){
-            $('.toggle-status').on('click', function(){
-                let id = $(this).data('id');
-                let name = $(this).data('name');
-                let status = $(this).prop('checked') ? 1 : 0;
 
+            $('#approve-input').on('change', function(){
+                $('#approve_form').submit();
+            });
+
+            $('#approve_form').on('submit', function(e){
+                e.preventDefault();
+
+                let data = $(this).serialize();
                 $.ajax({
-                    method: 'GET',
-                    url: "{{ route('admin.toggle-news-status') }}",
-                    data: {
-                        id:id,
-                        name:name,
-                        status:status
-                    },
+                    method: 'PUT',
+                    url: "{{ route('admin.approve.news') }}",
+                    data: data,
                     success: function(data){
                         if(data.status === 'success'){
                             Toast.fire({
                                 icon: 'success',
                                 title: data.message
                             })
+
+                            window.location.reload();
                         }
                     },
                     error: function(error){
